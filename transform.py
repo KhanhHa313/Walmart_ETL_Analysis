d02291543d0f7d1400e20e3c161860393227e1f8
Sarah Nguyen <kha02092005@gmail.com>
2026-05-04 22:53:20 +0930

    add the function that deal with negative or 0 markdown values based on input desired pct

--- transform.py
+++ transform.py
@@ -3,11 +3,13 @@ import pandas as pd
 from logging_set_up import setup_logging
 from extract import load_raw_data
 from config import MARKDOWN_COLS 
+import numpy as np
 
  
 # TRANSFORM FUNCTIONS
 def merge_tables(sales, stores, features):
     #Group the department sales in sales table by store and dates
+    logging.info("Proceed to merging the three tables")
     sales = sales.groupby(['Store', 'Date'])['Weekly_Sales'].sum ().reset_index()
     
     #Merge the table based on the dates available in the sales data, main metrics for this report
@@ -29,6 +31,9 @@ def merge_tables(sales, stores, features):
 
 def update_data_types(df):
     #Update all the dtypes across all the columns for later operations on them 
+
+    logging.info("Proceed to updating the data type for the merged data frame")
+
     df['Store'] = df['Store'].astype (int)
     df['Is_Holiday'] = df['Is_Holiday'].astype (bool)
 
@@ -41,6 +46,46 @@ def update_data_types(df):
     return df
 
 
+def fix_negative_or_zero_markdowns(df, pct = 0.05):
+    #Quick investigations reveal some negative and 0 mardown values which are supposed to have either Null or postive values
+    #Therefore, these functions will check the number of these 'problematic' problems, if its more than 5% of total number of columns
+    #this will return errors and ask to recheck the data and import again, if not, converted to Null values
+    #The default is 5% but can change according to users
+    logging.info("Proceed to looking into negative and zero markdown data")
+
+    total_records = len (df) 
+    neg_mds = (df[MARKDOWN_COLS] <= 0).sum()
+    n = len (neg_mds)
+
+    #Checking the number of 0 or negative markdown values for each type of markdown
+    for i in neg_mds.index:
+        if neg_mds[i] > total_records*pct:
+            n = n-1
+            logging.warning(f'{i} has more than {pct*100}% of negative markdown')
+
+    #Print out comments based on the results of each function 
+    if n == len(MARKDOWN_COLS):
+        logging.info(f'There is no markdown having more than {pct*100}% of negative')
+        logging.info('The negatives have then been converted to NaN')
+
+        df[MARKDOWN_COLS] = df[MARKDOWN_COLS].mask(
+            df[MARKDOWN_COLS] <= 0, np.nan)
+
+        logging.info("Negative and Zero markdown data records have been converted to NaN")
+
+        
+    elif n <= len(MARKDOWN_COLS)/2: 
+        logging.warning('Please check the data before proceeding')
+        
+    else:
+        logging.info(f'There is less than half of markdown types having more than {pct*100}% of negative each')
+        logging.info('The negative will then still be converted to NaN unless stated otherwise')
+        df[MARKDOWN_COLS] = df[MARKDOWN_COLS].mask(
+            df[MARKDOWN_COLS] <= 0, np.nan)
+        logging.info("Negative and Zero markdown data records have been converted to NaN!!")
+
+    return None
+
 
 # TEST RUN
 
@@ -49,7 +94,8 @@ if __name__ == "__main__":
     sales, stores, features = load_raw_data()
     df = merge_tables(sales, stores, features)
     df = update_data_types(df)
-    # df = fix_negative_markdowns(df)
+    fix_negative_or_zero_markdowns(df, pct = 0.05)
+ 
     # df = add_feature_flags(df)
     # validate_dataframe(df)
     print(f"Shape of dataframe: {df.shape}")

e109b1555ca2e956c0a37c2db6355f92a5d68e17
Sarah Nguyen <kha02092005@gmail.com>
2026-05-06 18:50:30 +0930

    add the quarter sale analysis

--- analysis_question/quarterly_pattern.py
+++ analysis_question/quarterly_pattern.py
@@ -1,9 +1,10 @@
+import os
 import logging 
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from utils.config import SALES_FILE, STORES_FILE, FEATURES_FILE
 from utils.logging_set_up import setup_logging
+from utils.config import OUTPUT_PATH
 
 def quarterly_sales_pattern(df):
     #How do sales move across quarters?
@@ -13,6 +14,7 @@ def quarterly_sales_pattern(df):
 
     
     #Draw visualisable plots
+    logging.info ("Drawing visualisable plot...")
     melted = quarter_sales.reset_index().melt (id_vars = 'Year', 
                                  value_vars = quarter_sales.columns,
                                   value_name = 'Median Sales',
@@ -25,11 +27,15 @@ def quarterly_sales_pattern(df):
               data = melted,
              hue = 'Year', palette='bright')
     plt.ticklabel_format(style='plain', axis='y')
+    logging.info ("Returning results and chart...")
+    logging.info ("Done the quarterly sale analysis across years!!")
     return quarter_sales, fig 
 
 # TEST RUN
 
 if __name__ == "__main__":
+    df = pd.read_csv(os.path.join(OUTPUT_PATH, 'transformed_dataset.csv'), 
+                 parse_dates=['Date'])
     setup_logging()
     quarterly_sales_pattern(df)
 
